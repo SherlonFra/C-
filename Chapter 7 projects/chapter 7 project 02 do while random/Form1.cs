0cf9f2d4e4f3008f91650ae68af084e91126d444
SherlonFra <80291777+SherlonFra@users.noreply.github.com>
2021-04-19 20:05:23 -0400

    do while

--- Chapter 7 projects/chapter 7 project 02 do while random/Form1.cs
+++ Chapter 7 projects/chapter 7 project 02 do while random/Form1.cs
@@ -12,9 +12,64 @@ namespace chapter_7_project_02_do_while_random
 {
     public partial class Form1 : Form
     {
+        const int MAXITERATIONS = 200000; // Limit loop passes
+        
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            bool flag;
+            int counter; // Pass counter
+            int max; // Max value for random number
+            int last;
+            int current;
+            Random randomNumber = new Random();
+            //========= Program Input Step ================
+            flag = int.TryParse(txtMax.Text, out max);
+            if (flag == false)
+            {
+                MessageBox.Show("Digit characters only.", "Input Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                txtMax.Focus();
+                return;
+            }
+            //======== Program Process Step ==============
+            counter = 0;
+            last = (int)randomNumber.Next(max);
+            do
+            {
+                current = randomNumber.Next(max);
+                if (last == current)
+
+                {
+                    break;
+                }
+                last = current;
+                counter++;
+            } while (counter < MAXITERATIONS);
+            //========= Program Output Step ==============
+            if (counter < MAXITERATIONS)
+            {
+                lblAnswer.Text = "It took " + counter.ToString() + " passes to match";
+            }
+            else
+            {
+                lblAnswer.Text = "No back-to-back match";
+            }
+        }
+        //============ Program Termination Step ========
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
+    
 }
