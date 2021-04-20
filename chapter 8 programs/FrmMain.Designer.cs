34dcf09171d4db359abbe8b386bb825824417a08
SherlonFra <80291777+SherlonFra@users.noreply.github.com>
2021-04-20 19:07:08 -0400

    chapter 8

---
+++ chapter 8 programs/FrmMain.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace chapter_8_programs
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtInput = new System.Windows.Forms.RichTextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lsvOutput = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add Text:";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 54);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(259, 151);
+            this.txtInput.TabIndex = 3;
+            this.txtInput.Text = "";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(12, 223);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(102, 23);
+            this.btnCalc.TabIndex = 4;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(165, 223);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // lsvOutput
+            // 
+            this.lsvOutput.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.lsvOutput.HideSelection = false;
+            this.lsvOutput.Location = new System.Drawing.Point(12, 316);
+            this.lsvOutput.Name = "lsvOutput";
+            this.lsvOutput.Size = new System.Drawing.Size(259, 186);
+            this.lsvOutput.TabIndex = 9;
+            this.lsvOutput.UseCompatibleStateImageBehavior = false;
+            this.lsvOutput.View = System.Windows.Forms.View.Details;
+            this.lsvOutput.SelectedIndexChanged += new System.EventHandler(this.lsvOutput_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Letter";
+            this.columnHeader1.Width = 110;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Count";
+            this.columnHeader2.Width = 115;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 514);
+            this.Controls.Add(this.lsvOutput);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.Text = "Count letters";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox txtInput;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ListView lsvOutput;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ chapter 8 programs/FrmMain.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chapter_8_programs
+{
+    public partial class FrmMain : Form
+    {
+        private const int MAXLETTERS = 26; // Symbolic constants
+        private const int MAXCHARS = MAXLETTERS -1;
+        private const int LETTERA = 65;
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            char oneLetter;
+            int index;
+            int i;
+            int length;
+            int[] count = new int[MAXLETTERS];
+            string input;
+            string buff;
+            length = txtInput.Text.Length;
+            if (length == 0) // Anything to count??
+            {
+                MessageBox.Show("You need to enter some text.", "Missing Input");
+                txtInput.Focus();
+                return;
+            }
+            input = txtInput.Text;
+            input = input.ToUpper();
+            for (i = 0; i < input.Length; i++) // Examine all letters.
+            {
+                oneLetter = input[i]; // Get a character
+                index = oneLetter - LETTERA; // Make into an index
+                if (index < 0 || index > MAXCHARS) // A letter??
+                    continue; // Nope.
+                count[index]++; // Yep.
+            }
+            ListViewItem which;
+            for (i = 0; i < MAXLETTERS; i++)
+            {
+                oneLetter = (char)(i + LETTERA);
+                which = new ListViewItem(oneLetter.ToString());
+                which.SubItems.Add(count[i].ToString());
+                lsvOutput.Items.Add(which);
+            }
+        }
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void lsvOutput_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
