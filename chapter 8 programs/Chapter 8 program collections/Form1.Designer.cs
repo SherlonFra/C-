6c828a30cdcacfd0227fb8308db98533df385b52
SherlonFra <80291777+SherlonFra@users.noreply.github.com>
2021-04-21 15:22:05 -0400

    chap 8

---
+++ chapter 8 programs/Chapter 8 program collections/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Chapter_8_program_collections
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lstTest = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(138, 12);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(108, 23);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(138, 53);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(108, 23);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lstTest
+            // 
+            this.lstTest.FormattingEnabled = true;
+            this.lstTest.ItemHeight = 16;
+            this.lstTest.Location = new System.Drawing.Point(12, 12);
+            this.lstTest.Name = "lstTest";
+            this.lstTest.Size = new System.Drawing.Size(120, 404);
+            this.lstTest.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 450);
+            this.Controls.Add(this.lstTest);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "Form1";
+            this.Text = "Collections";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ListBox lstTest;
+    }
+}
+
---
+++ chapter 8 programs/Chapter 8 program collections/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter_8_program_collections
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            int[] days = new int[] { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 31 };
+            string[] weekDays = new string[] { "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday","Sunday"};
+            foreach (string str in weekDays)
+            {
+                lstTest.Items.Add(str);
+            }
+            foreach (int val in days)
+            {
+                lstTest.Items.Add(val.ToString());
+            }
+        }
+    }
+}
