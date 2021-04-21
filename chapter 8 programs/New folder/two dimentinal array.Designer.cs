68c960b92e428a15f42e2ccabca5ceb949e2e5b7
SherlonFra <80291777+SherlonFra@users.noreply.github.com>
2021-04-20 20:39:11 -0400

    two dimensional array

---
+++ chapter 8 programs/New folder/two dimentinal array.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace chapter_8_programs
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtMax = new System.Windows.Forms.RichTextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lsvTable = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(204, 34);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(44, 22);
+            this.txtMax.TabIndex = 3;
+            this.txtMax.Text = "";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(3, 82);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(102, 23);
+            this.btnCalc.TabIndex = 4;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(140, 82);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // lsvTable
+            // 
+            this.lsvTable.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lsvTable.HideSelection = false;
+            this.lsvTable.Location = new System.Drawing.Point(3, 148);
+            this.lsvTable.Name = "lsvTable";
+            this.lsvTable.Size = new System.Drawing.Size(276, 339);
+            this.lsvTable.TabIndex = 9;
+            this.lsvTable.UseCompatibleStateImageBehavior = false;
+            this.lsvTable.View = System.Windows.Forms.View.Details;
+            this.lsvTable.SelectedIndexChanged += new System.EventHandler(this.lsvOutput_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "N";
+            this.columnHeader1.Width = 48;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "N*N";
+            this.columnHeader2.Width = 115;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "N*N*N";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Table to run from 0 through:";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 514);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lsvTable);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.Text = "Count letters";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox txtMax;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ListView lsvTable;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ chapter 8 programs/New folder/two dimentinal array.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chapter_8_programs
+{
+    public partial class FrmMain : Form
+    {
+        private const int MAXLETTERS = 26; // Symbolic constants
+        private const int MAXCHARS = MAXLETTERS -1;
+        private const int LETTERA = 65;
+        
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            bool flag;
+            int number;
+            int i;
+
+            ListViewItem which;
+            flag = int.TryParse(txtMax.Text, out number); // check input
+            if (flag == false)
+            {
+                MessageBox.Show("Numeric data only.", "Input Error");
+                txtMax.Focus();
+                return;
+            }
+            if (number < 0) // Make sure it's positive
+            {
+                number = number * 1;
+            }
+            number++; // Do this because of N - 1 Rule
+            int[,] myData = new int[number, 3]; // Define array
+            for (i = 0; i < number; i++)
+            {
+                myData[i, 0] = i; // first column of table
+                myData[i, 1] = i * i; // second column of table
+                myData[i, 2] = i * i * i; // third column of table
+            }
+            for (i = 0; i < number; i++) // Now show it
+            {
+                which = new ListViewItem(myData[i, 0].ToString());
+                which.SubItems.Add(myData[i, 1].ToString());
+                which.SubItems.Add(myData[i, 2].ToString());
+                lsvTable.Items.Add(which);
+            }
+        }
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void lsvOutput_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
