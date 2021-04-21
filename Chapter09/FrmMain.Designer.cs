7c97fb107abbed49ce821f363739f32bd317e118
SherlonFra <80291777+SherlonFra@users.noreply.github.com>
2021-04-21 18:54:54 -0400

    update

---
+++ Chapter09/FrmMain.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace Chapter09
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblLeapYearResult = new System.Windows.Forms.Label();
+            this.lblEasterResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Year:";
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(121, 43);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(100, 22);
+            this.txtYear.TabIndex = 1;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(23, 93);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(91, 23);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(134, 93);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblLeapYearResult
+            // 
+            this.lblLeapYearResult.AutoSize = true;
+            this.lblLeapYearResult.Location = new System.Drawing.Point(84, 145);
+            this.lblLeapYearResult.Name = "lblLeapYearResult";
+            this.lblLeapYearResult.Size = new System.Drawing.Size(46, 17);
+            this.lblLeapYearResult.TabIndex = 4;
+            this.lblLeapYearResult.Text = "label2";
+            // 
+            // lblEasterResult
+            // 
+            this.lblEasterResult.AutoSize = true;
+            this.lblEasterResult.Location = new System.Drawing.Point(84, 188);
+            this.lblEasterResult.Name = "lblEasterResult";
+            this.lblEasterResult.Size = new System.Drawing.Size(46, 17);
+            this.lblEasterResult.TabIndex = 5;
+            this.lblEasterResult.Text = "label3";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 275);
+            this.Controls.Add(this.lblEasterResult);
+            this.Controls.Add(this.lblLeapYearResult);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.Text = "Class Design";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblLeapYearResult;
+        private System.Windows.Forms.Label lblEasterResult;
+    }
+}
+
---
+++ Chapter09/clsDates.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter09
+{
+    class clsDates
+    {
+        private static int[] daysInMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        private int day;
+        private int month;
+        private int year;
+        private int leapYear;
+        private DateTime current;
+
+        public clsDates()
+        {
+            current = DateTime.Now; // Sets DateTime to right now
+        }
+
+        /*
+        * Purpose: To determine if the year is a leap year. Algorithm
+        * taken from C Programmer's Toolkit, Purdum, Que Corp.,
+        * 1993, p. 258.
+        *
+        * Parameter list:
+        * int year the year under consideration
+        *
+        * Return value:
+        * int 1 if a leap year, 0 otherwise
+        */
+
+        public int getLeapYear(int year)
+        {
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                return 1; // It is a leap year
+            else
+                return 0; // Nope.
+        }
+
+        /*
+        * Purpose: To determine the date for Easter given a year. Algorithm
+        * taken from C Programmer's Toolkit, Purdum, Que Corp.,
+        * 1993, p. 267.
+        *
+        * Parameter list:
+        * int year the year under consideration
+        *
+        * Return value:
+        * string the date in MM/DD/YYYY format
+        */
+
+        public string getEaster(int year)
+        {
+            int offset;
+            int leap;
+            int day;
+            int temp1;
+            int temp2;
+            int total;
+            offset = year % 19;
+            leap = year % 4;
+            day = year % 7;
+            temp1 = (19 * offset + 24) % 30;
+            temp2 = (2 * leap + 4 * day + 6 * temp1 + 5) % 7;
+            total = (22 + temp1 + temp2);
+            if (total > 31)
+            {
+                month = 4; // Easter is in April...
+                day = total - 31; // ...on this day
+            }
+            else
+            {
+                month = 3; // Easter is in March...
+                day = total; // ...on this day
+            }
+            DateTime myDT = new DateTime(year, month, day);
+            return myDT.ToLongDateString();
+        }
+
+
+
+    }
+}
