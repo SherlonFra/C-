ea66d138ad4efa3845837b9f2115743c315a20ed
SherlonFra <80291777+SherlonFra@users.noreply.github.com>
2021-04-26 19:10:36 -0400

    update

--- Chapter10program01/Chapter10 program 1/clsCardDeck.cs
+++ Chapter10program01/Chapter10 program 1/clsCardDeck.cs
@@ -8,32 +8,31 @@ namespace Chapter10_program_1
 {
     class clsCardDeck
     {
-        // =============== symbolic constants ==================
-        private const int DECKSIZE = 52; // The number of cards in the deck
-                                         // =============== static members ======================
+        
+        private const int DECKSIZE = 52; 
         private static string[] pips = {"",
         "AS","2S","3S","4S","5S","6s","7S","8S","9S","TS","JS","QS","KS",
         "AH","2H","3H","4H","5H","6H","7H","8H","9H","TH","JH","QH","KH",
         "AD","2D","3D","4D","5D","6D","7D","8D","9D","TD","JD","QD","KD",
         "AC","2C","3C","4C","5C","6C","7C","8C","9C","TC","JC","QC","KC"
         };
-        // =============== instance members =====================
-        private int nextCard; // The next card to be dealt from deck
-        private int[] deck = new int[DECKSIZE + 1]; // The deck of cards.
-        private int passCount; // To count loop passes to shuffle deck
-                               // =============== constructor(s) ======================
+       
+        private int nextCard; 
+        private int[] deck = new int[DECKSIZE + 1]; 
+        private int passCount;
+                              
         public clsCardDeck()
         {
             nextCard = 1;
         }
-        // =============== property methods =====================
+       
         public int DeckSize
         {
             get
             {
-                return DECKSIZE; // How many cards in the deck
+                return DECKSIZE; 
             }
-            // No setter method since this is a read-only property
+            
         }
         public int NextCard
         {
@@ -56,45 +55,29 @@ namespace Chapter10_program_1
                 return passCount;
             }
         }
-        // =============== helper methods =======================
-        // =============== general methods ======================
-        /**
-        * Purpose: Shuffle the deck
-        *
-        * Parameter list:
-        * N/A
-        * Return value:
-        * int number of passes to shuffle the deck
-*/
+  
         public int ShuffleDeck()
         {
             int index;
             int val;
             Random rnd = new Random();
-            passCount = 0; // Count how many times through the while loop
+            passCount = 0; 
             index = 1;
-            Array.Clear(deck, 0, deck.Length); // Initialize array to 0's
+            Array.Clear(deck, 0, deck.Length); 
             while (index < deck.Length)
-            { // Add 1 to offset 0-based arrays
-                val = rnd.Next(DECKSIZE) + 1; // Generate values 1 thru 52
+            { 
+                val = rnd.Next(DECKSIZE) + 1; 
                 if (deck[val] == 0)
-                { // Is this card place in the deck "unused"?
-                    deck[val] = index; // Yep, so assign it a card place
-                    index++; // Get ready for next card
+                { 
+                    deck[val] = index; 
+                    index++; 
                 }
                 passCount++;
             }
-            nextCard = 1; // Prepare to deal the first card
+            nextCard = 1; 
             return passCount;
         }
-        /**
-* Purpose: Show a given card in the deck.
-*
-* Parameter list:
-* int the index of the position where the card is found
-* Return value:
-* string the pip for the card, or empty on error
-*/
+
         public string getOneCard(int index)
         {
             if (index > 0 && index <= deck.Length && nextCard <= deck.Length)
@@ -104,18 +87,10 @@ namespace Chapter10_program_1
             }
             else
             {
-                return ""; // Error
+                return ""; 
             }
         }
-        /**
-        * Purpose: Show the abbreviation used for a given card in the deck.
-        *
-        * Parameter list:
-        * index an integer for the index position in the deck
-        *
-        * Return value:
-        * string the pip for the card, or empty on error
-*/
+
         public string getCardPip(int index)
         {
             if (index > 0 && index <= DECKSIZE)
@@ -124,7 +99,7 @@ namespace Chapter10_program_1
             }
             else
             {
-                return ""; // Error
+                return ""; 
             }
         }
     }
