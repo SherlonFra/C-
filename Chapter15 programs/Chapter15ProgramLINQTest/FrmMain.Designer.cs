59b0c215a2b32b8f4b7f14efe29aaf71555a4aef
SherlonFra <80291777+SherlonFra@users.noreply.github.com>
2021-05-10 18:36:04 -0400

    chap15

---
+++ Chapter15 programs/Chapter15ProgramLINQTest/FrmMain.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace Chapter15ProgramLINQTest
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstFull = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLow = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHi = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "List Of Random Values";
+            // 
+            // lstFull
+            // 
+            this.lstFull.FormattingEnabled = true;
+            this.lstFull.ItemHeight = 16;
+            this.lstFull.Location = new System.Drawing.Point(16, 47);
+            this.lstFull.Name = "lstFull";
+            this.lstFull.Size = new System.Drawing.Size(150, 340);
+            this.lstFull.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(196, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Find those value with a low limit of:";
+            // 
+            // txtLow
+            // 
+            this.txtLow.Location = new System.Drawing.Point(199, 47);
+            this.txtLow.Name = "txtLow";
+            this.txtLow.Size = new System.Drawing.Size(221, 22);
+            this.txtLow.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(234, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "And an Upper limit of:";
+            // 
+            // txtHi
+            // 
+            this.txtHi.Location = new System.Drawing.Point(255, 108);
+            this.txtHi.Name = "txtHi";
+            this.txtHi.Size = new System.Drawing.Size(100, 22);
+            this.txtHi.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(196, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(230, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "And display them in listbox on right:";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(255, 182);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(99, 23);
+            this.btnCalc.TabIndex = 7;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(268, 364);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(454, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Query Result:";
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.ItemHeight = 16;
+            this.lstOutput.Location = new System.Drawing.Point(457, 47);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(120, 340);
+            this.lstOutput.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 450);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtHi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLow);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstFull);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstFull;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLow;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox lstOutput;
+    }
+}
+
---
+++ Chapter15 programs/Chapter15ProgramLINQTest/FrmMain.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Chapter15ProgramLINQTest
+{
+    public partial class FrmMain : Form
+    {
+        private const int MAXNUM = 100;
+        static List<int> numbers = new List<int>(); 
+
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GenerateRandomValues();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            int lo;
+            int hi;
+            lstOutput.Items.Clear();
+            SetTheLimits(out lo, out hi);
+            DoLINQQuery(lo, hi);
+        }
+
+        private void DoLINQQuery(int lo, int hi)
+        {
+            var query = from p in numbers 
+                        where p > lo && p < hi
+                        select p;
+            foreach (var val in query) 
+            {
+                lstOutput.Items.Add(val.ToString());
+            }
+        }
+
+        private void SetTheLimits(out int lo, out int hi)
+        {
+            bool flag = int.TryParse(txtLow.Text, out lo); 
+            if (flag == false)
+            {
+                MessageBox.Show("Numeric only, 0 to 100", "Input Error");
+                txtLow.Focus();
+            }
+            flag = int.TryParse(txtHi.Text, out hi);
+            if (flag == false)
+            {
+                MessageBox.Show("Numeric only, 0 to 100", "Input Error");
+                txtHi.Focus();
+            }
+        }
+
+        private void GenerateRandomValues()
+        {
+            int temp;
+            DateTime current = DateTime.Now;
+            Random rnd = new Random((int)current.Ticks);
+            for (int i = 0; i < MAXNUM; i++) 
+            {
+                temp = rnd.Next(MAXNUM);
+                numbers.Add(temp); 
+                lstFull.Items.Add(temp.ToString());
+            }
+        }
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
